e399f6728bf475876cd6b53572c1b71a76f85d3b
paragmraw <paragm.raw@gmail.com>
2023-10-04 11:00:13 +0530

    inc1

---
+++ datasets/README.dataset.txt
@@ -0,0 +1,6 @@
+# Object Detection for Safe Lane Changing > 2023-09-06 11:21am
+https://universe.roboflow.com/dayananda-sagar-university-q7oox/object-detection-for-safe-lane-changing
+
+Provided by a Roboflow user
+License: MIT
+
---
+++ multi-stream.py
@@ -0,0 +1,14 @@
+import concurrent.futures
+
+def run_script(script_name):
+    try:
+        import subprocess
+        subprocess.run(['python', script_name], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error running {script_name}: {e}")
+
+if __name__ == "__main__":
+    script_list = ["PAST_SEM/Data_Science/ADAS-Blind-Spot-Detection/rear.py"]  # Add more file names as needed
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.map(run_script, script_list)
